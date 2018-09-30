3d31c95a4231eb21743493c7f121811295eeb33e
zhanglicheng66 <405328994@qq.com>
2018-09-30 21:32:46 +0800

    shi

---
+++ mofangbaidu.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+ keyword.oninput = function(e) {
+    var value = keyword.value
+    if(value){
+        xuanxiang.classList.add('active')
+    }else{
+        xuanxiang.classList.remove('active')
+    }
+}
+
